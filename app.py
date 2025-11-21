8a168227c4918ece0d88c34ba618dc4a2f786dc5
Anush Jain <aj2032005@gmail.com>
2025-11-21 11:30:12 +0530

    Added side bar with individual sumbits and text_area that leads to multiple rerun for entire scripts will replace with form instead

---
+++ app.py
@@ -0,0 +1,43 @@
+from data_manager import load_payload
+import streamlit as st 
+import json
+from data_manager import save_data
+
+st.title("Email Assistant")
+
+if "emails" not in st.session_state:
+    emails = load_payload("mock_inbox.json")
+    st.session_state["emails"] = emails
+
+if "prompts" not in st.session_state:
+    prompts = load_payload("prompts.json")
+    st.session_state["prompts"] = prompts
+
+st.sidebar.header("AI configration")
+
+action = st.sidebar.text_area(label="Action" , value = st.session_state["prompts"]["action_extraction"])
+action_button =  st.button("Save Prompt")
+if action_button and action:
+    st.session_state['prompts']["action_extraction"] = action
+    file = st.session_state["prompts"]
+    save_data("prompts.json",file)
+elif action_button and not action:
+    st.write("Please provide some text to submit.")
+
+category = st.sidebar.text_area(label="Categorization" , value = st.session_state["prompts"]["categorization"])
+category_button =  st.button("Save Prompt")
+if category_button and category:
+    st.session_state["prompts"]["categorization"] = category
+    file = st.session_state["prompts"]
+    save_data("prompts.json",file)
+elif category_button and not category:
+    st.write("Please provide some text to submit.")
+
+reply = st.sidebar.text_area(label="Auto-reply" , value = st.session_state["prompts"]["auto_reply"])
+reply_button =  st.button("Save Prompt")
+if reply_button and reply:
+    st.session_state["prompts"]["auto_reply"] = reply
+    file = st.session_state["prompts"]
+    save_data("prompts.json",file)
+elif reply_button and not reply:
+    st.write("Please provide some text to submit.")
